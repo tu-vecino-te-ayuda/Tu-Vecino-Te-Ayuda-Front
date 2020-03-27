6c1916309a1a6d90a34595456a3c082d3d475c5f
David <info@nesiweb.com>
2020-03-27 21:39:25 +0100

    Lazy load in images

--- www/js/default.js
+++ www/js/default.js
@@ -252,3 +252,27 @@ function aceptarCookies() {
     localStorage.aceptaCookies = 'true';
     cajacookies.style.display = 'none';
   }
+  
+// Code By Webdevtrick ( https://webdevtrick.com )
+(function() {
+	var elements = document.querySelectorAll('img[data-src]');
+	var index = 0;
+	var lazyLoad = function() {	
+		if(index >= elements.length) return;
+		var item = elements[index];	
+		if((this.scrollY + this.innerHeight) > item.offsetTop) {		
+		var src = item.getAttribute("data-src");
+		item.src = src;
+		item.addEventListener('load', function() {
+		item.removeAttribute('data-src');    	
+		});     
+		index++;
+		lazyLoad();
+		}
+	};
+	var init = function() {
+		window.addEventListener('scroll', lazyLoad);
+		lazyLoad();
+	};
+	return init();
+})();
