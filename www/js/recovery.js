dd9a0399aa226b25a3f09ec8951e12bd89dda324
Rodrigo Esgueva <esgueva@gmail.com>
2020-04-03 23:38:29 +0200

    init-fresh-style

--- www/js/recovery.js
+++
@@ -1,6 +0,0 @@
-$( document ).ready(function() {
-    let urlParams = new URLSearchParams(window.location.search);
-    if(!urlParams.has('email') || !urlParams.has('token')){
-        window.location.href="./index.html";
-    }
-});
