00a7be4baedcce90ab06d22d570fc138656e9046
Rodrigo Esgueva <esgueva@gmail.com>
2020-04-07 18:52:36 +0200

    feature/form-register-v2

--- www/js/register.js
+++ www/js/register.js
@@ -1,15 +1,34 @@
 var tempPassword = '';
+
 $( document ).ready(function() {
+    
+    initValues();
+
+    $('#form-help, #form-vol, #form-ent').submit(function( event ) {
+    
+        // Get data if it's validated
+        var data = getDataForm(event);
+
+        if ( data != null){
+            postData(data);
+        }
+
+    });
+});
+
+
+/** Init State & City */
+function initValues(){
     let ayudaprov = $('.ayuda-provincia');
     let ayudaciudad = $('.ayuda-ciudad');
 
     ayudaprov.empty();
     ayudaciudad.empty();
 
-    ayudaprov.append('<option selected="true" value="0" disabled>--Seleccione provincia--</option>');
+    ayudaprov.append('<option selected="true" value="" disabled>--Seleccione provincia--</option>');
     ayudaprov.prop('selectedIndex', 0);
 
-    ayudaciudad.append('<option selected="true" value="0" disabled>--Seleccione Ciudad--</option>');
+    ayudaciudad.append('<option selected="true" value="" disabled>--Seleccione Ciudad--</option>');
     ayudaciudad.prop('selectedIndex', 0);
 
     $.each(provincias, function (key, entry) {
@@ -20,7 +39,7 @@ $( document ).ready(function() {
         var id = $(this).val();
 
         ayudaciudad.empty();
-        ayudaciudad.append('<option selected="true" value="0" disabled>--Seleccione Ciudad--</option>');
+        ayudaciudad.append('<option selected="true" value="" disabled>--Seleccione Ciudad--</option>');
         ayudaciudad.prop('selectedIndex', 0);
 
         ayudaciudad.prop("disabled", false);
@@ -31,27 +50,166 @@ $( document ).ready(function() {
             }
         })          
       });
+}
 
-    $('#form-help, #form-vol, #form-ent').submit(function( event ) {
-        // grecaptcha.execute();
-        event.preventDefault(); 
-        var data = {};
-        var errors = '';
-        $.each(event.originalEvent.srcElement.elements, function(i, field) {
-            errors = errors + checkValues(field.name, field.value);
-            data[field.name] = field.value;
+/** Info Status Fields Form */
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        var forms = document.getElementsByClassName('needs-validation');
+        Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+            }, false);
         });
-        if(errors == ''){
-            postData($(this).attr('id'), data, event);
-        }else{
-            event.target.firstElementChild.innerHTML = errors;
+    }, false);
+})();
+
+/** Data Form */
+function getDataForm(event){
+    // grecaptcha.execute();
+    event.preventDefault(); 
+    var data = {};
+    var error = '';
+    
+    // Error Control
+    $.each(event.originalEvent.srcElement.elements, function(i, field) {
+        
+        error = checkValues(field.name, field.value);
+        
+        // Delete old notifications
+        $('.title-form .errorsNotification').remove()
+        $('.title-form').append("<div class='errorsNotification'> <div>")
+        
+        if (error != ''){
+            
+            // Add new notification
+            $('.title-form .errorsNotification').append("<p style='text-align:center; color:red'><strong>" + error + "</strong></p>");
+
+            //Exit each
+            return false
         }
+
+        //Load Data
+        data[field.name] = field.value;
     });
-});
+
+
+    // Warning CheckBox Legal
+    let legal = $('#checkLegal').is(":checked");
+    $('.user-form-check').css("color",legal ? "#212529" : "red");
+
+    // Error First
+    if (error != '' || !legal ) return null;
+    
+    // Validating All Data
+    return data;
+    
+}
+
+/** Check Values */
+function checkValues(key, value){
+    var estado = '';
+
+    switch(key){
+        case 'name':
+            if(value.length<3){
+                nameregex = /^[a-zA-Z ]+$/;
+                if (!nameregex.test(value)){
+                    estado = 'Error en: Nombre y Apellidos<br>';
+                }
+            }
+            break;
+        case 'email':
+            emailRegex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            if(!emailRegex.test(value)){
+                estado = 'Error en: Email<br>';
+            }
+            break;
+        case 'phone':
+            if(value.length<8 || value.length>12){
+                estado = 'Error en: Teléfono<br>';
+            }
+            break;
+        case 'password':
+            tempPassword = value;
+            if(value.length<8){
+                estado = 'Error en: Contraseña<br>';
+            }
+            break;
+        case 'password_confirmation':
+            if(value.length<8 || value != tempPassword){
+                estado = 'Error en: Las contraseñas son distintas<br>';
+            }
+            break;
+        case 'nearby_areas_id':
+            if(value == ''){
+                estado = 'Error en: Preferencias de desplazamiento<br>';
+            }
+            break;
+        case 'state':
+            if(value == ''){
+                estado = 'Error en: Provincia<br>';
+            }
+            break;
+        case 'city':
+            if(value == ''){
+                estado = 'Error en: Ciudad<br>';
+            }
+            break;
+        case 'address':
+            if(value.length<5){
+                estado = 'Error en: Dirección<br>';
+            }
+            break;
+        case 'zip_code':
+            if(value.length<5){
+                estado = 'Error en: Código Postal<br>';
+            }
+            break;
+        case 'cif':
+            if(value.length<2){
+                estado = 'Error en: CIF<br>';
+            }
+            break;
+        case 'corporate_name':
+            if(value.length<4){
+                estado = 'Error en: Nombre de la Asociación<br>';
+            }
+            break;
+        case 'activity_areas_id':
+            if(value == 0){
+                estado = 'Error en: Área de actividad<br>';
+            }
+            break;
+    }
+       
+
+    return estado;
+}
+
+/** Error Server */
+function errorServer(data){
+    //Reset Errors
+    $('.title-form .errorsNotification').remove()
+    $('.title-form').append("<div class='errorsNotification'> <div>")
+    //Print Errors
+    $.each(data, function(i, field) {
+        $('.title-form .errorsNotification').remove()
+        $('.title-form').append("<div class='errorsNotification'> <div>")
+        $('.title-form .errorsNotification').append("<p style='text-align:center; color:red'>" + field + "</p>");
+        $('input[name='+ i +']').focus();
+        $('input[name='+ i +']').val("");
+        return false;
+    });
+}
 
 /** Services */
-function postData(formId, data, event){
-    event.target.innerHTML = loader;
+function postData(data){
     $.ajax({
         type: "POST",
         url : server+'api/public/auth/register',
@@ -60,12 +218,9 @@ function postData(formId, data, event){
     }).then(function(data) {
         window.location.href="./registro-confirmado";
     }).fail(function(data) {
-        $(formId + ' .errors').html('Error en la inscripción, <a href="./">intentelo de nuevo</a>.<br>');
-
         if (data.responseJSON && data.responseJSON.errors) {
-            $.each(data.responseJSON.errors, function(i, field) {
-                $(formId + ' .errors').append('<b>' + i + '</b>: ' + field + '<br>');
-            });
+            errorServer(data.responseJSON.errors);
         }
     });
 };
+
